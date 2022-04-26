7e413436970e760e180503a1c2f2299df660b524
Nicolas Maalnica <nicolas.maalnica@gmail.com>
2022-04-26 00:13:20 -0300

    Primer desafio entregable. Ciclos/iteraciones. Funciones.

--- script.js
+++ script.js
@@ -1,23 +1,106 @@
 
-let entrada = true
+let cantidadEntrada
+let tipoEntrada
+let codigo = "locolope"
+let descuento = 1.3
 
-while (entrada) {
-    let condicion = prompt ("¿Ya tiene sus entradas asignadas?").toLowerCase ()
-    if (condicion == "si") {
+
+const valorTotal = (cantidadEntrada, tipoEntrada) => cantidadEntrada * tipoEntrada
+
+do {
+    alert("BIENVENIDO AL SISTEMA DE COMPRAS DE ENTRADAS")
+    let condicion = prompt ("¿Desea comprar entradas para ver el espectaculo?").toLowerCase ()
+    if (condicion == "no") {
     alert("¡Muchas gracias por usar nuestros servicios!")
-        entrada = false
-    } else if (condicion == "no"){
-        let valor = parseFloat(prompt ("¿Cuanto gastó en las entradas?")) 
-        if (valor <= 1001) {
-            alert("Puede sentarse entre las filas 5 a 10")
-            break
-        } else if (valor > 1000){
-            alert("Puede sentarse en las filas 1 a 4")
-            break
-        } else {
-            alert("Ingrese un importe válido")
-        }
-    } else {
-        alert("Ingrese una respesta válida")
+    break
+    } else if (condicion == "si"){
+    cantidadEntrada = parseFloat(prompt ("¿Cuantas entradas desea adquirir?"))
+    tipoEntrada = prompt("Ejija el tipo de entrada:\n(1) Entrada PREMIUM \n(2) Entrada STANDARD \n(3) Entrada LOWCOST")
+    }else {
+    alert("Ingrese una respuesta válida")
     }
-}
+} while (isNaN (cantidadEntrada) || isNaN (tipoEntrada))
+
+switch(tipoEntrada) {
+    case "1":
+        alert("El valor total de su compra es $"+valorTotal(cantidadEntrada, tipoEntrada = 1500))
+        codigo = prompt("Si posee un codigo promocional ingreselo a continuacion").toLowerCase()
+        if (codigo == "locolope") {
+            let valorFinal = (valorTotal(cantidadEntrada, tipoEntrada)/descuento).toFixed(2) 
+            alert(`El valor total de su compra es $${valorFinal}`)
+            let condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
+                if (condicion == "si"){
+                    alert("Muchas gracias por su compra")
+                } else if(condicion == "no"){
+                    alert("Miralo por TV")
+                } else {
+                    alert("Ingrese una respuesta válida")
+                }
+            
+        } else {
+            alert(`No ha ingresado ningún codigo de descuento válido.\nEl valor total es de $${valorTotal(cantidadEntrada, tipoEntrada)}`)
+            condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
+                if (condicion == "si"){
+                    alert("Muchas gracias por su compra")
+                } else if(condicion == "no"){
+                    alert("Miralo por TV")
+                } else {
+                    alert("Ingrese una respuesta válida")
+                }
+        }
+        break
+    case "2":
+        alert("El valor total de su compra es $"+valorTotal(cantidadEntrada, tipoEntrada = 1000))
+        codigo = prompt("Si posee un codigo promocional ingreselo a continuacion").toLowerCase()
+        if (codigo == "locolope") {
+            let valorFinal = (valorTotal(cantidadEntrada, tipoEntrada)/descuento).toFixed(2) 
+            alert(`El valor total de su compra es $${valorFinal}`)
+            let condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
+                if (condicion == "si"){
+                    alert("Muchas gracias por su compra")
+                } else if(condicion == "no"){
+                    alert("Miralo por TV")
+                } else {
+                    alert("Ingrese una respuesta válida")
+                }
+            
+        } else {
+            alert(`No ha ingresado ningún codigo de descuento válido.\nEl valor total es de $${valorTotal(cantidadEntrada, tipoEntrada)}`)
+            condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
+                if (condicion == "si"){
+                    alert("Muchas gracias por su compra")
+                } else if(condicion == "no"){
+                    alert("Miralo por TV")
+                } else {
+                    alert("Ingrese una respuesta válida")
+                }
+        }
+        break
+    case "3":
+        alert("El valor total de su compra es $"+valorTotal(cantidadEntrada, tipoEntrada = 750))
+        codigo = prompt("Si posee un codigo promocional ingreselo a continuacion").toLowerCase()
+        if (codigo == "locolope") {
+            let valorFinal = (valorTotal(cantidadEntrada, tipoEntrada)/descuento).toFixed(2) 
+            alert(`El valor total de su compra es $${valorFinal}`)
+            let condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
+                if (condicion == "si"){
+                    alert("Muchas gracias por su compra")
+                } else if(condicion == "no"){
+                    alert("Miralo por TV")
+                } else {
+                    alert("Ingrese una respuesta válida")
+                }
+            
+        } else {
+            alert(`No ha ingresado ningún codigo de descuento válido.\nEl valor total es de $${valorTotal(cantidadEntrada, tipoEntrada)}`)
+            condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
+                if (condicion == "si"){
+                    alert("Muchas gracias por su compra")
+                } else if(condicion == "no"){
+                    alert("Miralo por TV")
+                } else {
+                    alert("Ingrese una respuesta válida")
+                }
+        }
+        break
+    }
