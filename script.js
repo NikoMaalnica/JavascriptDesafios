758f37eb6dca6495898e54d6ea707f7fbeac3cb2
Nicolas Maalnica <nicolas.maalnica@gmail.com>
2022-05-18 21:59:19 -0300

    2da Prentrega. LOCAL STORAGE. JSON

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let tipoEntrada
 let codigo = "locolope"
 let descuento = 1.3
 
+const valorTotal = (cantidadEntrada, tipoEntrada) => cantidadEntrada * tipoEntrada
+
 //OBJETOS ENTRADAS
 
 class Entrada {
@@ -16,9 +18,6 @@ class Entrada {
         this.beneficioEntrada = beneficioEntrada
         this.valorEntrada = valorEntrada,
         this.stockEntrada = stockEntrada
-    }
-    entradasVendidas (nuevaCantidad){ //Metodo para disminuir la cantidad de entradas vendidas
-        this.stockEntrada -= nuevaCantidad
     }
 }
 
@@ -33,7 +32,7 @@ console.log(entradas)
 
 // DOM - CREANDO LAS CARDS
 
-let divCards = document.getElementById('divCards')
+let divCards = document.querySelector('#divCards')
 
 entradas.forEach (entrada => {
     divCards.innerHTML += `
@@ -55,172 +54,46 @@ entradas.forEach (entrada => {
     `
 })
 
-// EVENTO CLICK - AGREGANDO ENTRADAS AL MODAL
+//LOCAL STORAGE
+
+let carrito = []
+
+if (localStorage.getItem('carrito')){
+    // JSON A OBJETO
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+} else {
+    //OBJETO A JSON
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+// EVENTO CLICK - AGREGANDO ENTRADAS AL MODAL Y AL CARRITO DEL LOCAL STORAGE
 
 entradas.forEach (entrada => {
-    let btnId = document.getElementById (`btn${entrada.id}`)
-    let divEntrada = document.getElementById (`codigo${entrada.id}`)
-    let bodyModal = document.getElementById ('bodyModal')
+    let btnId = document.querySelector (`#btn${entrada.id}`)
+    let bodyModal = document.querySelector ('#bodyModal')
     btnId.addEventListener('click', () => {
         bodyModal.innerHTML += `
-    <div class="card mb-3" style="max-width: 540px;">
-        <div class="row g-0">
-            <div class="col-md-4 divImgModal">
-                <img src="${entrada.imagenEntrada}" class="modalImg rounded d-block rounded-start" alt="Imagen del club">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">${entrada.nombreEntrada}</h5>
-                    <p class="card-text">${entrada.descripcionEntrada}</p>
-                    <ul class="list-group list-group-flush mb-2">
-                        <li class="list-group-item">${entrada.beneficioEntrada}</li>
-                        <li class="list-group-item">Valor: $${entrada.valorEntrada}</li>
-                        <li class="list-group-item">Stock disponible: ${entrada.stockEntrada}</li>
-                    </ul>
+        <div class="card mb-3" style="max-width: 540px;">
+            <div id="modalDiv${entrada.id}"class="row g-0">
+                <div class="col-md-4 divImgModal">
+                    <img src="${entrada.imagenEntrada}" class="modalImg rounded d-block rounded-start" alt="Imagen del club">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${entrada.nombreEntrada}</h5>
+                        <p class="card-text">${entrada.descripcionEntrada}</p>
+                        <ul class="list-group list-group-flush mb-2">
+                            <li class="list-group-item">${entrada.beneficioEntrada}</li>
+                            <li class="list-group-item">Valor: $${entrada.valorEntrada}</li>
+                            <li class="list-group-item">Stock disponible: ${entrada.stockEntrada}</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    `
+        `
+        let entradaEnCarrito = entrada
+        carrito.push (entradaEnCarrito)
+        localStorage.setItem ('carrito', JSON.stringify(carrito))
     })
 })
-
-
-/*
-
-// EVENTO PARA AGREGAR AL MODAL CLONANDO 
-
-entradas.forEach (entrada => {
-    let btnId = document.getElementById (`btn${entrada.id}`)
-    let divEntrada = document.getElementById (`codigo${entrada.id}`)
-    let bodyModal = document.getElementById ('bodyModal')
-    let clone = divEntrada.cloneNode (true);
-    btnId.addEventListener('click', () => {
-        bodyModal.appendChild (clone)
-        }
-    )
-})
-
-
-*/
-
-
-const valorTotal = (cantidadEntrada, tipoEntrada) => cantidadEntrada * tipoEntrada
-
-//INTERACCION CON EL USUARIO
-
-/*
-
-do {
-    alert("BIENVENIDO AL SISTEMA DE COMPRAS DE ENTRADAS")
-    let condicion = prompt ("¿Desea comprar entradas para ver el espectaculo?").toLowerCase ()
-    if (condicion == "no") {
-    alert("¡Muchas gracias por usar nuestros servicios!")
-    break
-    } else if (condicion == "si"){
-    cantidadEntrada = parseFloat(prompt ("¿Cuantas entradas desea adquirir?"))
-    tipoEntrada = prompt("Ejija el tipo de entrada:\n(1) VIP Ticket \n(2) PREMIUM Ticket \n(3) Standard Ticket")
-    }else {
-    alert("Ingrese una respuesta válida")
-    }
-} while (isNaN (cantidadEntrada) || isNaN (tipoEntrada))
-
-switch(tipoEntrada) {
-    case "1":
-        alert("El valor total de su compra es $"+valorTotal(cantidadEntrada, tipoEntrada = 1500))
-        codigo = prompt("Si posee un codigo promocional ingreselo a continuacion").toLowerCase()
-        if (codigo == "locolope") {
-            let valorFinal = (valorTotal(cantidadEntrada, tipoEntrada)/descuento).toFixed(2) 
-            alert(`El valor total de su compra es $${valorFinal}`)
-            let condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
-                if (condicion == "si"){
-                    entrada1.entradasVendidas (cantidadEntrada)
-                    alert("Muchas gracias por su compra")
-                } else if(condicion == "no"){
-                    alert("Miralo por TV")
-                } else {
-                    alert("Ingrese una respuesta válida")
-                }
-            
-        } else {
-            alert(`No ha ingresado ningún codigo de descuento válido.\nEl valor total es de $${valorTotal(cantidadEntrada, tipoEntrada)}`)
-            condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
-                if (condicion == "si"){
-                    entrada1.entradasVendidas (cantidadEntrada)
-                    alert("Muchas gracias por su compra")
-                } else if(condicion == "no"){
-                    alert("Miralo por TV")
-                } else {
-                    alert("Ingrese una respuesta válida")
-                }
-        }
-        break
-    case "2":
-        alert("El valor total de su compra es $"+valorTotal(cantidadEntrada, tipoEntrada = 1000))
-        codigo = prompt("Si posee un codigo promocional ingreselo a continuacion").toLowerCase()
-        if (codigo == "locolope") {
-            let valorFinal = (valorTotal(cantidadEntrada, tipoEntrada)/descuento).toFixed(2) 
-            alert(`El valor total de su compra es $${valorFinal}`)
-            let condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
-                if (condicion == "si"){
-                    entrada2.entradasVendidas (cantidadEntrada)
-                    alert("Muchas gracias por su compra")
-                } else if(condicion == "no"){
-                    alert("Miralo por TV")
-                } else {
-                    alert("Ingrese una respuesta válida")
-                }
-            
-        } else {
-            alert(`No ha ingresado ningún codigo de descuento válido.\nEl valor total es de $${valorTotal(cantidadEntrada, tipoEntrada)}`)
-            condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
-                if (condicion == "si"){
-                    entrada2.entradasVendidas (cantidadEntrada)
-                    alert("Muchas gracias por su compra")
-                } else if(condicion == "no"){
-                    alert("Miralo por TV")
-                } else {
-                    alert("Ingrese una respuesta válida")
-                }
-        }
-        break
-    case "3":
-        alert("El valor total de su compra es $"+valorTotal(cantidadEntrada, tipoEntrada = 750))
-        codigo = prompt("Si posee un codigo promocional ingreselo a continuacion").toLowerCase()
-        if (codigo == "locolope") {
-            let valorFinal = (valorTotal(cantidadEntrada, tipoEntrada)/descuento).toFixed(2) 
-            alert(`El valor total de su compra es $${valorFinal}`)
-            let condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
-                if (condicion == "si"){
-                    entrada3.entradasVendidas (cantidadEntrada)
-                    alert("Muchas gracias por su compra")
-                } else if(condicion == "no"){
-                    alert("Miralo por TV")
-                } else {
-                    alert("Ingrese una respuesta válida")
-                }
-            
-        } else {
-            alert(`No ha ingresado ningún codigo de descuento válido.\nEl valor total es de $${valorTotal(cantidadEntrada, tipoEntrada)}`)
-            condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
-                if (condicion == "si"){
-                    entrada3.entradasVendidas (cantidadEntrada)
-                    alert("Muchas gracias por su compra")
-                } else if(condicion == "no"){
-                    alert("Miralo por TV")
-                } else {
-                    alert("Ingrese una respuesta válida")
-                }
-        }
-        break
-}
-
-*/
-
-//ARRAY QUE MUESTRA EL TIPO DE ENTRADA Y LA CANTIDAD RESTANTE
-
-let entradasDisponibles = [entrada1.nombreEntrada, entrada1.stockEntrada, entrada2.nombreEntrada, entrada2.stockEntrada, entrada3.nombreEntrada, entrada3.stockEntrada]
-
-for(let entradaEnArray of entradasDisponibles) {
-    console.log (entradaEnArray)
-}
--- scriptPrueba.js
+++ scriptPrueba.js
@@ -32,6 +32,8 @@ comprador1.saludar()*/
 
 //objeto para los tipos de entradas con metodo para restar las entradas vendidas
 
+/*
+
 class Entrada {
     constructor(nombreEntrada, valorEntrada, stockEntrada){
         this.nombreEntrada = nombreEntrada,
@@ -52,7 +54,7 @@ entrada1.entradasVendidas (3)
 console.log (entrada1)
 
 // Arrays
-/*
+
 let array = [true, 5, "Nico", false, 1000]
 
 console.log (array)
@@ -60,8 +62,151 @@ console.log (array)
 console.log (array.length)
 for (let i = 0; i < array.length; i++) {
     console.log (array[i])
-}*/
-
+}
 let entradasEnArray = [entrada1, entrada2, entrada3]
 
 console.log (entradasEnArray)
+
+*/
+
+//INTERACCION CON EL USUARIO
+
+/*
+
+//entradasVendidas > Metodo adentro del Class
+entradasVendidas (nuevaCantidad){ //Metodo para disminuir la cantidad de entradas vendidas
+        this.stockEntrada -= nuevaCantidad
+    }
+
+do {
+    alert("BIENVENIDO AL SISTEMA DE COMPRAS DE ENTRADAS")
+    let condicion = prompt ("¿Desea comprar entradas para ver el espectaculo?").toLowerCase ()
+    if (condicion == "no") {
+    alert("¡Muchas gracias por usar nuestros servicios!")
+    break
+    } else if (condicion == "si"){
+    cantidadEntrada = parseFloat(prompt ("¿Cuantas entradas desea adquirir?"))
+    tipoEntrada = prompt("Ejija el tipo de entrada:\n(1) VIP Ticket \n(2) PREMIUM Ticket \n(3) Standard Ticket")
+    }else {
+    alert("Ingrese una respuesta válida")
+    }
+} while (isNaN (cantidadEntrada) || isNaN (tipoEntrada))
+
+switch(tipoEntrada) {
+    case "1":
+        alert("El valor total de su compra es $"+valorTotal(cantidadEntrada, tipoEntrada = 1500))
+        codigo = prompt("Si posee un codigo promocional ingreselo a continuacion").toLowerCase()
+        if (codigo == "locolope") {
+            let valorFinal = (valorTotal(cantidadEntrada, tipoEntrada)/descuento).toFixed(2) 
+            alert(`El valor total de su compra es $${valorFinal}`)
+            let condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
+                if (condicion == "si"){
+                    entrada1.entradasVendidas (cantidadEntrada)
+                    alert("Muchas gracias por su compra")
+                } else if(condicion == "no"){
+                    alert("Miralo por TV")
+                } else {
+                    alert("Ingrese una respuesta válida")
+                }
+            
+        } else {
+            alert(`No ha ingresado ningún codigo de descuento válido.\nEl valor total es de $${valorTotal(cantidadEntrada, tipoEntrada)}`)
+            condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
+                if (condicion == "si"){
+                    entrada1.entradasVendidas (cantidadEntrada)
+                    alert("Muchas gracias por su compra")
+                } else if(condicion == "no"){
+                    alert("Miralo por TV")
+                } else {
+                    alert("Ingrese una respuesta válida")
+                }
+        }
+        break
+    case "2":
+        alert("El valor total de su compra es $"+valorTotal(cantidadEntrada, tipoEntrada = 1000))
+        codigo = prompt("Si posee un codigo promocional ingreselo a continuacion").toLowerCase()
+        if (codigo == "locolope") {
+            let valorFinal = (valorTotal(cantidadEntrada, tipoEntrada)/descuento).toFixed(2) 
+            alert(`El valor total de su compra es $${valorFinal}`)
+            let condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
+                if (condicion == "si"){
+                    entrada2.entradasVendidas (cantidadEntrada)
+                    alert("Muchas gracias por su compra")
+                } else if(condicion == "no"){
+                    alert("Miralo por TV")
+                } else {
+                    alert("Ingrese una respuesta válida")
+                }
+            
+        } else {
+            alert(`No ha ingresado ningún codigo de descuento válido.\nEl valor total es de $${valorTotal(cantidadEntrada, tipoEntrada)}`)
+            condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
+                if (condicion == "si"){
+                    entrada2.entradasVendidas (cantidadEntrada)
+                    alert("Muchas gracias por su compra")
+                } else if(condicion == "no"){
+                    alert("Miralo por TV")
+                } else {
+                    alert("Ingrese una respuesta válida")
+                }
+        }
+        break
+    case "3":
+        alert("El valor total de su compra es $"+valorTotal(cantidadEntrada, tipoEntrada = 750))
+        codigo = prompt("Si posee un codigo promocional ingreselo a continuacion").toLowerCase()
+        if (codigo == "locolope") {
+            let valorFinal = (valorTotal(cantidadEntrada, tipoEntrada)/descuento).toFixed(2) 
+            alert(`El valor total de su compra es $${valorFinal}`)
+            let condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
+                if (condicion == "si"){
+                    entrada3.entradasVendidas (cantidadEntrada)
+                    alert("Muchas gracias por su compra")
+                } else if(condicion == "no"){
+                    alert("Miralo por TV")
+                } else {
+                    alert("Ingrese una respuesta válida")
+                }
+            
+        } else {
+            alert(`No ha ingresado ningún codigo de descuento válido.\nEl valor total es de $${valorTotal(cantidadEntrada, tipoEntrada)}`)
+            condicion =prompt ("¿Desea realizar la compra?").toLowerCase ()
+                if (condicion == "si"){
+                    entrada3.entradasVendidas (cantidadEntrada)
+                    alert("Muchas gracias por su compra")
+                } else if(condicion == "no"){
+                    alert("Miralo por TV")
+                } else {
+                    alert("Ingrese una respuesta válida")
+                }
+        }
+        break
+}
+
+*/
+
+/*
+//ARRAY QUE MUESTRA EL TIPO DE ENTRADA Y LA CANTIDAD RESTANTE
+
+let entradasDisponibles = [entrada1.nombreEntrada, entrada1.stockEntrada, entrada2.nombreEntrada, entrada2.stockEntrada, entrada3.nombreEntrada, entrada3.stockEntrada]
+
+for(let entradaEnArray of entradasDisponibles) {
+    console.log (entradaEnArray)
+}
+*/
+
+/*
+
+// EVENTO PARA AGREGAR AL MODAL CLONANDO 
+
+entradas.forEach (entrada => {
+    let btnId = document.getElementById (`btn${entrada.id}`)
+    let divEntrada = document.getElementById (`codigo${entrada.id}`)
+    let bodyModal = document.getElementById ('bodyModal')
+    let clone = divEntrada.cloneNode (true);
+    btnId.addEventListener('click', () => {
+        bodyModal.appendChild (clone)
+        }
+    )
+})
+
+*/
